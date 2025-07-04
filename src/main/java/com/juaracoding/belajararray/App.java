2d5f6e0a03b2d23f8da742d927bb0b1f6b593b70
adireska <adireskamauladi04@gmail.com>
2025-07-04 21:38:34 +0700

    Update Class Array

--- src/main/java/com/juaracoding/belajararray/App.java
+++ src/main/java/com/juaracoding/belajararray/App.java
@@ -5,16 +5,21 @@ public class App
     public static void main( String[] args )
     {
         // Ver 1 Array
-        int kontrakan[] = new int[8];
-        int hargaPerOrang = 300000;
-        kontrakan[0] = 4;
-        kontrakan[1] = 2;
-        kontrakan[2] = 2;
-        kontrakan[3] = 7;
-        kontrakan[4] = 2;
-        kontrakan[5] = 6;
-        kontrakan[6] = 1;
-        kontrakan[7] = 3;
+        // int kontrakan[] = new int[8];
+        
+        // int kontrakan[] = {4,2,2,7,0,0,1,3};
+        // int hargaPerOrang = 300000;
+        // int pendapatanTotal = 0;
+        // int index = 0;
+
+        // kontrakan[0] = 4;
+        // kontrakan[1] = 2;
+        // kontrakan[2] = 2;
+        // kontrakan[3] = 7;
+        // kontrakan[4] = 2;
+        // kontrakan[5] = 6;
+        // kontrakan[6] = 1;
+        // kontrakan[7] = 3;
 
         // System.out.println(kontrakan[0]);
         // System.out.println(kontrakan[1]);
@@ -24,7 +29,6 @@ public class App
         // System.out.println(kontrakan[5]);
         // System.out.println(kontrakan[6]);
         // System.out.println(kontrakan[7]);
-
 
         // int homeLoan[] = {4,2,2,7,2,6,1,3};
         // System.out.println(homeLoan[0]);
@@ -36,12 +40,54 @@ public class App
         // System.out.println(homeLoan[6]);
         // System.out.println(homeLoan[7]);
 
-        for (int index = 0; index < kontrakan.length; index++) {
-            System.out.println(kontrakan[index] * hargaPerOrang);
-        
+        // for (int index = 0; index < kontrakan.length; index++) {
+        //     if (kontrakan[index] == 0) {
+        //         continue;
+        //     }
+        //     if (kontrakan[index] % 4 == 0) {
+        //         kontrakan[index] = kontrakan[index] +2;
+        //     }
+        //     pendapatanTotal += kontrakan[index]
+        // }
+
+        // for (int tampung : kontrakan) {
+
+        //     if (tampung == 0) {
+        //         continue;
+        //     }else {
+        //         index++;
+        //     }
+
+        //     if (tampung % 4 == 0) {
+        //         kontrakan[index] = kontrakan[index] + 2;
+        //     }
+
+        //     pendapatanTotal += tampung * hargaPerOrang;
+        // }
+
+        // System.out.println("Pendapatan per bulan: " + pendapatanTotal);
+
+        int [][] warZone = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9},
+        };
+        // System.out.println(warZone[1][0]);
+        // System.out.println(warZone[2][2]);
+
+
+        for (int row = 0; row < warZone.length; row++) {
+            for (int col = 0; col < warZone[row].length; col++){
+                if (warZone[row][col] % 3 == 0) {
+                    warZone[row][col] = warZone[row][col] % 3;       
+                }
+            }
         }
-
-
-
+        for (int[] row: warZone){
+            for (int col:row){
+                System.out.println(col);
+            }
+            
+        };
     }
 }
